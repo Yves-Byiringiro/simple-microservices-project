87e0595e3128c2c330d0b869d311e61437a41f64
Yves <ybyiring@andrew.cmu.edu>
2023-05-01 10:24:40 +0200

    first commit

---
+++ event-bus/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:8000/events", event);
+  axios.post('http://localhost:8001/events', event);
+  axios.post("http://localhost:8003/events", event);
+
+  res.send({ status: "OK" });
+});
+
+
+
+app.listen(8002, () => {
+  console.log("Listening on port 8002");
+});
