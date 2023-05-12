c00ac59d45e32c66238d3bf2f3ce67a1745713da
Yves <ybyiring@andrew.cmu.edu>
2023-05-12 06:54:52 +0200

    rm unwanted comments in query service

--- query/index.js
+++ query/index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const axios = require("axios");
-const ObjectId = mongoose.Types.ObjectId;
 
 const config = require('./config/mongodb.json');
 const { Post } = require("./models/post");
